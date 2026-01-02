7f5947e9a48b1b354febc3c2dfc2e15486d8fdd3
aiman.fahmi <aiman.fahmi@beans.com.my>
2026-01-02 15:31:47 +0800

    update mapstruct with new config

--- src/main/java/com/trackwize/authentication/mapstruct/TokenMapStruct.java
+++ src/main/java/com/trackwize/authentication/mapstruct/TokenMapStruct.java
@@ -2,9 +2,10 @@ package com.trackwize.authentication.mapstruct;
 
 import com.trackwize.authentication.model.dto.TokenReqDTO;
 import com.trackwize.authentication.model.entity.Token;
+import com.trackwize.common.config.MapStructConfig;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", config = MapStructConfig.class)
 public interface TokenMapStruct {
 
     Token toEntity(TokenReqDTO reqDTO);
--- src/main/java/com/trackwize/authentication/mapstruct/UserMapStruct.java
+++ src/main/java/com/trackwize/authentication/mapstruct/UserMapStruct.java
@@ -2,9 +2,10 @@ package com.trackwize.authentication.mapstruct;
 
 import com.trackwize.authentication.model.dto.UserRegistrationReqDTO;
 import com.trackwize.authentication.model.entity.User;
+import com.trackwize.common.config.MapStructConfig;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", config = MapStructConfig.class)
 public interface UserMapStruct {
 
     User toEntity(UserRegistrationReqDTO reqDTO);
